de2dde6615982cc15dc906ce6a867a67993a4f12
birundha55 <birunthakrishnan555@gmail.com>
2024-10-01 13:51:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{color:"red",backgroundColor:"gray"}}>BIRUNDHA</h1>
+        <h3>WEB DEVELOPER</h3>
+        <hr></hr>
+        <h5>179,annanagar valaiyapatti</h5>
+        <h5>birunthakrishnan555@gmail.com</h5>
+        <h5>8428643742</h5>
+        <hr></hr>
+        <h3>PROFILE</h3>
+        <p>
+         I am qualified and professional web developer.strong creative andanalytical skills.team player with an eye for details.seeking for an entry level opportunities with an esteemed organisation where i can utilise my skills and enhance learning in the field of work.
+         </p>
+         <hr></hr>
+           <h3>TECHNICAL SKILLS</h3>
+        <ul>
+             <li>html</li>
+             <li>css</li>
+             <li>python</li>
+             <li>javascript</li>
+        </ul>
+        <hr></hr>
+        <h3>EDUCATION</h3>
+        <h4>Bachelor of Engineering</h4>
+        <h4>Vetri Vinayaha College of Engineering and Technology</h4>
+        <hr></hr>
+        <h3>SKILLS</h3>
+        <ul>  
+            <li>Ieadership</li>
+            <li>Discipline</li>
+            <li>Slfe-confident</li>
+        </ul>
+        <hr></hr>
+        <h3>PROJECT</h3>
+        <h4>portifolio</h4>
+        <p>
+            The portifolio isdeveloped using reactjs a popular javascript library for buildinguser interface .Additionally technogy include html,css.
+            <hr></hr>
+            <h3>CERTIFICATION</h3>
+            <h4>Full stack development</h4>
+            <h5>Novitech I coimbatore</h5>  
+         </p>   
+    </div>
+  );
+}
+
+export default App;
